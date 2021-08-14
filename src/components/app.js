8aade9d72d1c3bcd29f5e00cf1d8f8ad684fed4c
eayalarod <eayalarod@gmail.com>
2021-08-13 23:16:39 -0400

    blank slate

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Scott Muzic Marketing Website</h1>
+        <h2>Livingston County Electrician</h2>
       </div>
     );
   }
